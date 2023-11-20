5a2d1d4178e60532e896c19cf0c3047df6cbc7c8
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-20 11:36:15 -0300

    post movies endpoint

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
 
 const app = express()
 app.disable('x-powered-by')
+
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('<h1>Hola</h1>')
@@ -10,8 +13,12 @@ app.get('/', (req, res)=>{
 
 app.get('/movies', (req, res)=>{
     const { genre } = req.query
-    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase()))
-    if (genre) return res.json(filteredMovies)
+    console.log(genre)
+    
+    if (genre){
+        const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase()))
+        return res.json(filteredMovies)
+    } 
     res.json(movies)
 })
 
@@ -20,6 +27,24 @@ app.get('/movies/:id', (req, res)=>{
     const movie = movies.find((movie => movie.id === id))
     if (movie) return res.json(movie)
     res.status(404).json({message: 'not found'})
+})
+
+app.post('/movies', (req, res) => {
+    const {title, genre, year, director, duration, rate, poster} = req.body
+
+    const newMovies = {
+        id: crypto.randomUUID(),
+        title,
+        genre,
+        year,
+        director,
+        duration,
+        rate: rate ?? 0,
+        poster
+    } 
+
+    movies.push(newMovies)
+    res.status(201).json(newMovies)
 })
 
 app.use((req, res)=>{
